524b642c22b4fa67e0ed61f2fe664eab5b7bc972
BLARD <justin.blard@epitech.eu>
2022-05-26 00:44:31 +0100

    [INIT] MUSIC MOD IS HERE
    This is the first version with no custom music added.

---
+++ src/data/scripts/plugin/MusicPlugin.java
@@ -0,0 +1,34 @@
+/*
+** Script by Laroustine
+*/
+package data.scripts.plugin;
+
+import com.fs.starfarer.api.campaign.FactionAPI;
+import com.fs.starfarer.api.combat.CombatEngineAPI;
+import com.fs.starfarer.api.impl.MusicPlayerPluginImpl;
+
+import org.apache.log4j.Logger;
+
+public class MusicPlugin extends MusicPlayerPluginImpl {
+    protected final Logger LOG = Logger.getLogger(MusicPlugin.class);
+
+    @Override
+    public String getMusicSetIdForCombat(CombatEngineAPI engine) {
+        if (engine.isInCampaign()) {
+            FactionAPI faction = engine.getContext().getOtherFleet().getFaction();
+            String musicId = faction.getMusicMap().get("battle");
+
+            if (musicId != null) {
+                LOG.info("The music for this faction is : " + musicId);
+                return musicId;
+            }
+            LOG.info("The Faction [" + faction.getDisplayName() + "] does not have custom music.");
+        } else if (engine.isMission()) {
+            LOG.info("The Mission [" + engine.getMissionId() + "] does not have custom music.");
+            LOG.info("Missions don't have anyway of having custom music yet.");
+        } else {
+            LOG.info("Music is set to default.");
+        }
+        return super.getMusicSetIdForCombat(engine);
+    }
+}
